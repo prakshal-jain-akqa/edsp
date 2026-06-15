df4ff70b247f58dca8ce387e9ff3956168218684
prakshal-jain-akqa <prakshal.jain@akqa.com>
2026-06-15 15:47:31 +0530

    meta auhtor attempt 4

--- blocks/metadata/metadata.js
+++
@@ -1,7 +0,0 @@
-/**
- * The Metadata block is processed server-side by the EDS pipeline.
- * @param {Element} block The metadata block element
- */
-export default function decorate(block) {
-  block.classList.add('metadata-processed');
-}
